cc940f49a9156d73219f8c887df2e7bd7c029611
Justin Lietz <justingagelietz@gmail.com>
2024-11-21 22:20:03 +0000

    added comments and tests

--- libs/qec/include/cudaq/qec/codes/surface_code.h
+++ libs/qec/include/cudaq/qec/codes/surface_code.h
@@ -14,8 +14,11 @@ using namespace cudaqx;
 
 namespace cudaq::qec::surface_code {
 
-enum surface_role { data, amx, amz, empty };
+/// @brief enumerates the role of a grid site in the surface codes stabilizer
+/// grid
+enum surface_role { amx, amz, empty };
 
+/// @brief describes the 2d coordinate on the stabilizer grid
 struct vec2d {
   int row;
   int col;
@@ -26,72 +29,123 @@ struct vec2d {
 vec2d operator+(const vec2d &lhs, const vec2d &rhs);
 vec2d operator-(const vec2d &lhs, const vec2d &rhs);
 bool operator==(const vec2d &lhs, const vec2d &rhs);
-// impose 2d ordering for reproducibility
 bool operator<(const vec2d &lhs, const vec2d &rhs);
 
-// Grid layout from left to right, top to bottom
-// (0,0)   (0,1)   (0,2)   (0,3)   (0,4)
-// (1,0)   (1,1)   (1,2)   (1,3)   (1,4)
-// (2,0)   (2,1)   (2,2)   (2,3)   (2,4)
-// (3,0)   (3,1)   (3,2)   (3,3)   (3,4)
-// (4,0)   (4,1)   (4,2)   (4,3)   (4,4)
-
-// Represents the x & z stabilizers in the rotated surface code,
-// including data about their layout in a 2D grid.
+/// @brief Generates and keeps track of the 2d grid of stabilizers in the
+/// rotated surface code
+// Grid layout is arranged from left to right, top to bottom (row major storage)
+// grid_length = 4 example:
+// (0,0)   (0,1)   (0,2)   (0,3)
+// (1,0)   (1,1)   (1,2)   (1,3)
+// (2,0)   (2,1)   (2,2)   (2,3)
+// (3,0)   (3,1)   (3,2)   (3,3)
+//
+// Each entry on the grid can be an X stabilizer, Z stabilizer,
+// or empty, as is needed on the edges.
+// The grid length of 4 corresponds to a distance 3 surface code, which results
+// in: e(0,0)  e(0,1)  Z(0,2)  e(0,3) X(1,0)  Z(1,1)  X(1,2)  e(1,3) e(2,0)
+// X(2,1)  Z(2,2)  X(2,3) e(3,0)  Z(3,1)  e(3,2)  e(3,3)
+//
+// This is seen through the `print_stabilizer_grid()` member function.
+// To get rid of the empty sites, the `print_stabilizer_coords()` function is
+// used:
+//                 Z(0,2)
+// X(1,0)  Z(1,1)  X(1,2)
+//         X(2,1)  Z(2,2)  X(2,3)
+//         Z(3,1)
+//
+// and to get the familiar visualization of the distance three surface code, the
+// `print_stabilizer_indices` results in:
+//         Z0
+// X0  Z1  X1
+//     X2  Z2  X3
+//     Z3
+//
+// The data qubits are located at the four corners of each of the weight-4
+// stabilizers. They are also organized with index increasing from left to
+// right, top to bottom: d0  d1  d2 d3  d4  d5 d6  d7  d8
 class stabilizer_grid {
-  public:
-  // The distance of the code,
+private:
+  /// @brief Generates this->roles
+  void generate_grid_roles();
+  /// @brief Generates {x,z}_stab_coords and indices
+  void generate_grid_indices();
+  /// @brief Generates {x,z}_stabilizers
+  void generate_stabilizers();
+
+public:
+  /// @brief The distance of the code,
   // determines the number of data qubits per dimension
   uint32_t distance;
 
-  // length of the grid
+  /// @brief length of the stabilizer grid
   // for distance = d data qubits,
   // the stabilizer grid has length d+1
   uint32_t grid_length;
+
+  /// @brief flattened vector of the stabilizer grid sites roles'
   // grid idx -> role
   // stored in row major order
   std::vector<surface_role> roles;
-  // stab index -> 2d coord
-  std::vector<vec2d> z_stab_coords;
+
+  /// @brief x stab index -> 2d coord
   std::vector<vec2d> x_stab_coords;
-  // 2d coord -> stab index
+
+  /// @brief z stab index -> 2d coord
+  std::vector<vec2d> z_stab_coords;
+
+  /// @brief 2d coord -> z stab index
   std::map<vec2d, size_t> x_stab_indices;
+
+  /// @brief 2d coord -> z stab index
   std::map<vec2d, size_t> z_stab_indices;
 
-  // data qubits are in an offset 2D coord system
-  // data index -> 2d coord
+  /// @brief data index -> 2d coord
+  // data qubits are in an offset 2D coord system from stabilizers
   std::vector<vec2d> data_coords;
-  // 2d coord -> data index
+
+  /// @brief 2d coord -> data index
   std::map<vec2d, size_t> data_indices;
 
+  /// @brief Each element is an X stabilizer specified by the data qubits it has
+  /// support on
   // In surface code, can have weight 2 or weight 4 stabs
   // So {x,z}_stabilizer[i].size() == 2 || 4
   std::vector<std::vector<size_t>> x_stabilizers;
+
+  /// @brief Each element is an Z stabilizer specified by the data qubits it has
+  /// support on
   std::vector<std::vector<size_t>> z_stabilizers;
 
-  // The distance determines how many data qubits are on an edge.
-  // The grid itself has stabilizers extending beyond this data
-  // qubit edge on each side, as well as many empty grid sites
-  // along this edge.
-
-  // Construct the grid from the code's distance
+  /// @brief Construct the grid from the code's distance
   stabilizer_grid(uint32_t distance);
-  // Empty constructor
+  /// @brief Empty constructor
   stabilizer_grid();
 
-  void generate_grid_roles();
-  void generate_grid_indices();
-  void generate_stabilizers();
+  /// @brief Print a 2d grid of stabilizer roles
+  void print_stabilizer_grid() const;
 
-  void print_stabilizer_coords();
-  void print_stabilizer_indices();
-  void print_stabilizer_maps();
-  void print_data_grid();
-  void print_stabilizer_grid();
-  void print_stabilizers();
+  /// @brief Print a 2d grid of stabilizer coords
+  void print_stabilizer_coords() const;
+
+  /// @brief Print a 2d grid of stabilizer indices
+  void print_stabilizer_indices() const;
+
+  /// @brief Print a 2d grid of data qubit indices
+  void print_data_grid() const;
+
+  /// @brief Print the coord <--> indices maps
+  void print_stabilizer_maps() const;
+
+  /// @brief Print the stabilizers in sparse pauli format
+  void print_stabilizers() const;
+
+  /// @brief Get the stabilizers as a vector of cudaq::spin_ops
+  std::vector<cudaq::spin_op> get_spin_op_stabilizers() const;
+
+  /// @brief Get the observables as a vector of cudaq::spin_ops
+  std::vector<cudaq::spin_op> get_spin_op_observables() const;
 };
-
-
 
 /// \pure_device_kernel
 ///
@@ -209,13 +263,12 @@ public:
   /// @brief Extension creator function for the surface_code
   CUDAQ_EXTENSION_CUSTOM_CREATOR_FUNCTION(
       surface_code, static std::unique_ptr<cudaq::qec::code> create(
-                  const cudaqx::heterogeneous_map &options) {
+                        const cudaqx::heterogeneous_map &options) {
         return std::make_unique<surface_code>(options);
       })
 
   /// @brief Grid to keep track of topological arrangement of qubits.
   stabilizer_grid grid;
-
 };
 
 } // namespace cudaq::qec::surface_code
