02b9b631749f6062b1f9507d97fa416afd6f530e
Ben Howe <bhowe@nvidia.com>
2025-01-09 01:41:11 +0000

    Activate Python wheel testing - attempt 1
    Signed-off-by: Ben Howe <bhowe@nvidia.com>

--- scripts/ci/test_wheels.sh
+++ scripts/ci/test_wheels.sh
@@ -12,27 +12,25 @@
 set -e
 
 # Installing dependencies
-python_version=3.10
+python_version=$1
 python=python${python_version}
 
 apt-get update && apt-get install -y --no-install-recommends \
         libgfortran5 python${python_version} python$(echo ${python_version} | cut -d . -f 1)-pip
 
-${python} -m pip install --no-cache-dir pytest nvidia-cublas-cu11
+${python} -m pip install --no-cache-dir pytest
 
-cd /cuda-qx
-
-${python} -m pip install wheels/cuda_quantum_cu12-0.0.0-cp310-cp310-manylinux_2_28_x86_64.whl
+#${python} -m pip install wheels/cuda_quantum_cu12-0.0.0-cp310-cp310-manylinux_2_28_x86_64.whl
 
 # QEC library
 # ======================================
 
-${python} -m pip install wheels/cudaq_qec-0.0.1-cp310-cp310-*.whl
+${python} -m pip install /wheels/cudaq_qec-*.whl
 ${python} -m pytest libs/qec/python/tests/
 
 # Solvers library
 # ======================================
 
-${python} -m pip install wheels/cudaq_solvers-0.0.1-cp310-cp310-*.whl
+${python} -m pip install /wheels/cudaq_solvers-*.whl
 ${python} -m pytest libs/solvers/python/tests/
 
