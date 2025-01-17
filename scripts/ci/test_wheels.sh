9bdbde764e579d84396b86439f4a244c3f20d858
Ben Howe <bhowe@nvidia.com>
2025-01-17 20:45:31 +0000

    Fix path
    Signed-off-by: Ben Howe <bhowe@nvidia.com>

--- scripts/ci/test_wheels.sh
+++ scripts/ci/test_wheels.sh
@@ -28,11 +28,11 @@ ${python} -m pip install --no-cache-dir pytest
 # QEC library
 # ======================================
 
-${python} -m pip install wheels/cudaq_qec-*-cp${python_version_no_dot}-cp${python_version_no_dot}-*.whl
+${python} -m pip install /wheels/cudaq_qec-*-cp${python_version_no_dot}-cp${python_version_no_dot}-*.whl
 ${python} -m pytest libs/qec/python/tests/
 
 # Solvers library
 # ======================================
 
-${python} -m pip install wheels/cudaq_solvers-*-cp${python_version_no_dot}-cp${python_version_no_dot}-*.whl
+${python} -m pip install /wheels/cudaq_solvers-*-cp${python_version_no_dot}-cp${python_version_no_dot}-*.whl
 ${python} -m pytest libs/solvers/python/tests/
