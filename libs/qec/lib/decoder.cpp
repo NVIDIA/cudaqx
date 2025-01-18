35c14df3106952e1aebe3920cbe51027589255f7
Ben Howe <bhowe@nvidia.com>
2025-01-18 00:34:05 +0000

    Rename variable
    Signed-off-by: Ben Howe <bhowe@nvidia.com>

--- libs/qec/lib/decoder.cpp
+++ libs/qec/lib/decoder.cpp
@@ -81,9 +81,9 @@ std::unique_ptr<decoder> get_decoder(const std::string &name,
 // Constructor function for auto-loading plugins
 __attribute__((constructor)) void load_decoder_plugins() {
   // Load plugins from the decoder-specific plugin directory
-  std::filesystem::path cudaqLibPath{cudaqx::__internal__::getCUDAQXLibraryPath(
+  std::filesystem::path libPath{cudaqx::__internal__::getCUDAQXLibraryPath(
       cudaqx::__internal__::CudaQXLibType::QEC)};
-  auto pluginPath = cudaqLibPath.parent_path() / "decoder-plugins";
+  auto pluginPath = libPath.parent_path() / "decoder-plugins";
   load_plugins(pluginPath.string(), PluginType::DECODER);
 }
 
