8a83f43f52c72f4e0cf97a93a2ea77bcc3e51f00
Ben Howe <bhowe@nvidia.com>
2025-01-19 17:26:01 +0000

    Cleanup
    Signed-off-by: Ben Howe <bhowe@nvidia.com>

--- scripts/ci/test_wheels.sh
+++ scripts/ci/test_wheels.sh
@@ -39,7 +39,7 @@ fi
 # ======================================
 
 ${python} -m pip install /wheels/cudaq_qec-*-cp${python_version_no_dot}-cp${python_version_no_dot}-*.whl
-${python} -m pytest -s libs/qec/python/tests/test_decoder.py::test_decoder_plugin_initialization # libs/qec/python/tests/
+${python} -m pytest -s libs/qec/python/tests/
 
 # Solvers library
 # ======================================
