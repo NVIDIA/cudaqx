bff8818b15bca6c483df1f3e9610ecc408228593
kvmto <kmato@nvidia.com>
2025-02-21 23:31:05 +0000

    Tests refactored and reset
    Signed-off-by: kvmto <kmato@nvidia.com>

--- libs/solvers/unittests/test_adapt.cpp
+++ libs/solvers/unittests/test_adapt.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 NVIDIA Corporation & Affiliates.                         *
+ * Copyright (c) 2024 - 2025 NVIDIA Corporation & Affiliates.                  *
  * All rights reserved.                                                        *
  *                                                                             *
  * This source code and the accompanying materials are made available under    *
@@ -14,12 +14,23 @@
 #include "cudaq/solvers/adapt.h"
 #include "cudaq/solvers/operators.h"
 
+bool check_gpu_available() {
+  static int gpu_available = -1; // -1 means unknown; 0 = no; 1 = yes
+  if (gpu_available < 0) {
+    if (std::system("nvidia-smi > /dev/null 2>&1") != 0)
+      gpu_available = 0;
+    else
+      gpu_available = 1;
+  }
+  return static_cast<bool>(gpu_available);
+}
+
 class SolversTester : public ::testing::Test {
 protected:
-  cudaq::spin_op h;
-  cudaq::spin_op hamli;
-  cudaq::spin_op hamhh;
-  void SetUp() override {
+  static cudaq::spin_op h;
+  static cudaq::spin_op hamli;
+  static cudaq::spin_op hamhh;
+  static void SetUpTestSuite() {
     std::vector<double> h2_data{
         3, 1, 1, 3, 0.0454063,  0,  2, 0, 0, 0, 0.17028,    0,
         0, 0, 2, 0, -0.220041,  -0, 1, 3, 3, 1, 0.0454063,  0,
@@ -29,7 +40,7 @@ protected:
         0, 2, 0, 2, 0.1202,     0,  2, 0, 0, 2, 0.165607,   0,
         0, 2, 2, 0, 0.165607,   0,  0, 0, 2, 2, 0.174073,   0,
         1, 1, 3, 3, -0.0454063, -0, 15};
-    this->h = cudaq::spin_op(h2_data, 4);
+    h = cudaq::spin_op(h2_data, 4);
 
     cudaq::solvers::molecular_geometry geometryLiH = {{"Li", {0.3925, 0., 0.}},
                                                       {"H", {-1.1774, 0., 0.}}};
@@ -46,97 +57,129 @@ protected:
     hamhh = hh.hamiltonian;
   }
 };
+cudaq::spin_op SolversTester::h;
+cudaq::spin_op SolversTester::hamli;
+cudaq::spin_op SolversTester::hamhh;
 
-TEST_F(SolversTester, checkSimpleAdapt) {
+TEST_F(SolversTester, checkSimpleAdapt_H2) {
   auto pool = cudaq::solvers::operator_pool::get("spin_complement_gsd");
-
   auto poolList = pool->generate({{"num-orbitals", h.num_qubits() / 2}});
-  auto [energy1, thetash1, ops1] = cudaq::solvers::adapt_vqe(
+  auto [energy, thetash, ops] = cudaq::solvers::adapt_vqe(
       hartreeFock2Electrons, h, poolList,
       {{"grad_norm_tolerance", 1e-2}, {"verbose", true}});
-  EXPECT_NEAR(energy1, -1.13, 1e-2);
-
-  poolList = pool->generate({{"num-orbitals", hamhh.num_qubits() / 2}});
-  auto [energy3, thetas3, ops3] = cudaq::solvers::adapt_vqe(
-      hartreeFock2Electrons, hamhh, poolList,
-      {{"grad_norm_tolerance", 1e-2}, {"verbose", true}});
-  EXPECT_NEAR(energy3, -1.13, 1e-2);
-
-  poolList = pool->generate({{"num-orbitals", hamli.num_qubits() / 2}});
-  auto [energy2, thetas2, ops2] = cudaq::solvers::adapt_vqe(
-      statePrep4Electrons, hamli, poolList,
-      {{"grad_norm_tolerance", 0.5}, {"verbose", true}});
-  EXPECT_NEAR(energy2, -7.88, 1e-2);
+  EXPECT_NEAR(energy, -1.13, 1e-2);
 }
 
-TEST_F(SolversTester, checkSimpleAdaptGradient) {
+TEST_F(SolversTester, checkSimpleAdapt_H2Sto3g) {
+  auto pool = cudaq::solvers::operator_pool::get("spin_complement_gsd");
+  auto poolList = pool->generate({{"num-orbitals", hamhh.num_qubits() / 2}});
+  auto [energy, thetas, ops] = cudaq::solvers::adapt_vqe(
+      hartreeFock2Electrons, hamhh, poolList,
+      {{"grad_norm_tolerance", 1e-2}, {"verbose", true}});
+  EXPECT_NEAR(energy, -1.13, 1e-2);
+}
+
+TEST_F(SolversTester, checkSimpleAdapt_LiHSto3g) {
+  if (!check_gpu_available())
+    GTEST_SKIP() << "No GPU available, skipping test because CPU is slow";
+  auto pool = cudaq::solvers::operator_pool::get("spin_complement_gsd");
+  auto poolList = pool->generate({{"num-orbitals", hamli.num_qubits() / 2}});
+  auto [energy, thetas, ops] = cudaq::solvers::adapt_vqe(
+      statePrep4Electrons, hamli, poolList,
+      {{"grad_norm_tolerance", 0.5}, {"verbose", true}});
+  EXPECT_NEAR(energy, -7.88, 1e-2);
+}
+
+TEST_F(SolversTester, checkSimpleAdaptGradient_H2) {
   auto pool = cudaq::solvers::operator_pool::get("spin_complement_gsd");
   auto opt = cudaq::optim::optimizer::get("lbfgs");
 
   auto poolList = pool->generate({{"num-orbitals", h.num_qubits() / 2}});
-  auto [energy1, thetas1, ops1] = cudaq::solvers::adapt_vqe(
+  auto [energy, thetas, ops] = cudaq::solvers::adapt_vqe(
       hartreeFock2Electrons, h, poolList, *opt, "central_difference",
       {{"grad_norm_tolerance", 1e-2}, {"verbose", true}});
-  EXPECT_NEAR(energy1, -1.13, 1e-2);
-  for (std::size_t i = 0; i < thetas1.size(); i++)
-    printf("%lf -> %s\n", thetas1[i], ops1[i].to_string().c_str());
+  EXPECT_NEAR(energy, -1.13, 1e-2);
+  for (std::size_t i = 0; i < thetas.size(); i++)
+    printf("%lf -> %s\n", thetas[i], ops[i].to_string().c_str());
+}
 
-  poolList = pool->generate({{"num-orbitals", hamhh.num_qubits() / 2}});
-  auto [energy3, thetas3, ops3] = cudaq::solvers::adapt_vqe(
+TEST_F(SolversTester, checkSimpleAdaptGradient_H2Sto3g) {
+  auto pool = cudaq::solvers::operator_pool::get("spin_complement_gsd");
+  auto opt = cudaq::optim::optimizer::get("lbfgs");
+
+  auto poolList = pool->generate({{"num-orbitals", hamhh.num_qubits() / 2}});
+  auto [energy, thetas, ops] = cudaq::solvers::adapt_vqe(
       hartreeFock2Electrons, hamhh, poolList, *opt, "central_difference",
       {{"grad_norm_tolerance", 1e-2}, {"verbose", true}});
-  EXPECT_NEAR(energy3, -1.13, 1e-2);
-  for (std::size_t i = 0; i < thetas3.size(); i++)
-    printf("%lf -> %s\n", thetas3[i], ops3[i].to_string().c_str());
+  EXPECT_NEAR(energy, -1.13, 1e-2);
+  for (std::size_t i = 0; i < thetas.size(); i++)
+    printf("%lf -> %s\n", thetas[i], ops[i].to_string().c_str());
+}
 
-  poolList = pool->generate({{"num-orbitals", hamli.num_qubits() / 2}});
-  auto [energy2, thetas2, ops2] = cudaq::solvers::adapt_vqe(
+TEST_F(SolversTester, checkSimpleAdaptGradient_LiHSto3g) {
+  if (!check_gpu_available())
+    GTEST_SKIP() << "No GPU available, skipping test because CPU is slow";
+  auto pool = cudaq::solvers::operator_pool::get("spin_complement_gsd");
+  auto opt = cudaq::optim::optimizer::get("cobyla");
+
+  auto poolList = pool->generate({{"num-orbitals", hamli.num_qubits() / 2}});
+  auto [energy, thetas, ops] = cudaq::solvers::adapt_vqe(
       statePrep4Electrons, hamli, poolList, *opt, "central_difference",
       {{"grad_norm_tolerance", 0.5}, {"verbose", true}});
-  EXPECT_NEAR(energy2, -7.88, 1e-2);
-  for (std::size_t i = 0; i < thetas2.size(); i++)
-    printf("%lf -> %s\n", thetas2[i], ops2[i].to_string().c_str());
+  EXPECT_NEAR(energy, -7.88, 1e-2);
+  for (std::size_t i = 0; i < thetas.size(); i++)
+    printf("%lf -> %s\n", thetas[i], ops[i].to_string().c_str());
 }
 
-TEST_F(SolversTester, checkSimpleAdaptUCCSD) {
+TEST_F(SolversTester, checkSimpleAdaptUCCSD_H2) {
   auto pool = cudaq::solvers::operator_pool::get("uccsd");
 
-  heterogeneous_map config1;
-  config1.insert("num-qubits", h.num_qubits());
-  config1.insert("num-electrons", 2);
-  auto poolList = pool->generate(config1);
-  auto [energy1, thetas1, ops1] = cudaq::solvers::adapt_vqe(
+  heterogeneous_map config;
+  config.insert("num-qubits", h.num_qubits());
+  config.insert("num-electrons", 2);
+  auto poolList = pool->generate(config);
+  auto [energy, thetas, ops] = cudaq::solvers::adapt_vqe(
       hartreeFock2Electrons, h, poolList,
       {{"grad_norm_tolerance", 1e-2}, {"verbose", true}});
-  EXPECT_NEAR(energy1, -1.13, 1e-2);
-
-  heterogeneous_map config3;
-  config3.insert("num-qubits", hamhh.num_qubits());
-  config3.insert("num-electrons", 2);
-  poolList = pool->generate(config3);
-  auto [energy3, thetas3, ops3] = cudaq::solvers::adapt_vqe(
-      hartreeFock2Electrons, hamhh, poolList,
-      {{"grad_norm_tolerance", 1e-2}, {"verbose", true}});
-  EXPECT_NEAR(energy3, -1.13, 1e-2);
-
-  heterogeneous_map config2;
-  config2.insert("num-qubits", hamli.num_qubits());
-  config2.insert("num-electrons", 4);
-  poolList = pool->generate(config2);
-  auto [energy2, thetas2, ops2] = cudaq::solvers::adapt_vqe(
-      statePrep4Electrons, hamli, poolList,
-      {{"grad_norm_tolerance", 0.5}, {"verbose", true}});
-  EXPECT_NEAR(energy2, -7.88, 1e-2);
+  EXPECT_NEAR(energy, -1.13, 1e-2);
 }
 
-TEST_F(SolversTester, checkSimpleAdaptGradientUCCSD) {
+TEST_F(SolversTester, checkSimpleAdaptUCCSD_H2Sto3g) {
+  auto pool = cudaq::solvers::operator_pool::get("uccsd");
+
+  heterogeneous_map config;
+  config.insert("num-qubits", hamhh.num_qubits());
+  config.insert("num-electrons", 2);
+  auto poolList = pool->generate(config);
+  auto [energy, thetas, ops] = cudaq::solvers::adapt_vqe(
+      hartreeFock2Electrons, hamhh, poolList,
+      {{"grad_norm_tolerance", 1e-2}, {"verbose", true}});
+  EXPECT_NEAR(energy, -1.13, 1e-2);
+}
+
+TEST_F(SolversTester, checkSimpleAdaptUCCSD_LiHSto3g) {
+  if (!check_gpu_available())
+    GTEST_SKIP() << "No GPU available, skipping test because CPU is slow";
+  auto pool = cudaq::solvers::operator_pool::get("uccsd");
+
+  heterogeneous_map config;
+  config.insert("num-qubits", hamli.num_qubits());
+  config.insert("num-electrons", 4);
+  auto poolList = pool->generate(config);
+  auto [energy, thetas, ops] = cudaq::solvers::adapt_vqe(
+      statePrep4Electrons, hamli, poolList,
+      {{"grad_norm_tolerance", 0.5}, {"verbose", true}});
+  EXPECT_NEAR(energy, -7.88, 1e-2);
+}
+
+TEST_F(SolversTester, checkSimpleAdaptGradientUCCSD_H2) {
   auto pool = cudaq::solvers::operator_pool::get("uccsd");
   auto opt = cudaq::optim::optimizer::get("lbfgs");
 
-  heterogeneous_map config1;
-  config1.insert("num-qubits", h.num_qubits());
-  config1.insert("num-electrons", 2);
-  auto poolList = pool->generate(config1);
+  heterogeneous_map config;
+  config.insert("num-qubits", h.num_qubits());
+  config.insert("num-electrons", 2);
+  auto poolList = pool->generate(config);
   auto [energy, thetas, ops] = cudaq::solvers::adapt_vqe(
       hartreeFock2Electrons, h, poolList, *opt, "central_difference",
       {{"grad_norm_tolerance", 1e-2}, {"verbose", true}});
@@ -144,28 +187,39 @@ TEST_F(SolversTester, checkSimpleAdaptGradientUCCSD) {
 
   for (std::size_t i = 0; i < thetas.size(); i++)
     printf("%lf -> %s\n", thetas[i], ops[i].to_string().c_str());
+}
+TEST_F(SolversTester, checkSimpleAdaptGradientUCCSD_H2Sto3g) {
+  auto pool = cudaq::solvers::operator_pool::get("uccsd");
+  auto opt = cudaq::optim::optimizer::get("lbfgs");
 
-  heterogeneous_map config3;
-  config3.insert("num-qubits", hamhh.num_qubits());
-  config3.insert("num-electrons", 2);
-  poolList = pool->generate(config3);
-  auto [energy3, thetas3, ops3] = cudaq::solvers::adapt_vqe(
+  heterogeneous_map config;
+  config.insert("num-qubits", hamhh.num_qubits());
+  config.insert("num-electrons", 2);
+  auto poolList = pool->generate(config);
+  auto [energy, thetas, ops] = cudaq::solvers::adapt_vqe(
       hartreeFock2Electrons, hamhh, poolList, *opt, "central_difference",
       {{"grad_norm_tolerance", 1e-2}, {"verbose", true}});
-  EXPECT_NEAR(energy3, -1.13, 1e-2);
+  EXPECT_NEAR(energy, -1.13, 1e-2);
 
-  for (std::size_t i = 0; i < thetas3.size(); i++)
-    printf("%lf -> %s\n", thetas3[i], ops3[i].to_string().c_str());
+  for (std::size_t i = 0; i < thetas.size(); i++)
+    printf("%lf -> %s\n", thetas[i], ops[i].to_string().c_str());
+}
 
-  heterogeneous_map config2;
-  config2.insert("num-qubits", hamli.num_qubits());
-  config2.insert("num-electrons", 4);
-  poolList = pool->generate(config2);
-  auto [energy2, thetas2, ops2] = cudaq::solvers::adapt_vqe(
+TEST_F(SolversTester, checkSimpleAdaptGradientUCCSD_LiHSto3g) {
+  if (!check_gpu_available())
+    GTEST_SKIP() << "No GPU available, skipping test because CPU is slow";
+
+  auto pool = cudaq::solvers::operator_pool::get("uccsd");
+  auto opt = cudaq::optim::optimizer::get("cobyla");
+  heterogeneous_map config;
+  config.insert("num-qubits", hamli.num_qubits());
+  config.insert("num-electrons", 4);
+  auto poolList = pool->generate(config);
+  auto [energy, thetas, ops] = cudaq::solvers::adapt_vqe(
       statePrep4Electrons, hamli, poolList, *opt, "central_difference",
       {{"grad_norm_tolerance", 0.5}, {"verbose", true}});
-  EXPECT_NEAR(energy2, -7.88, 1e-2);
+  EXPECT_NEAR(energy, -7.88, 1e-2);
 
-  for (std::size_t i = 0; i < thetas2.size(); i++)
-    printf("%lf -> %s\n", thetas2[i], ops2[i].to_string().c_str());
+  for (std::size_t i = 0; i < thetas.size(); i++)
+    printf("%lf -> %s\n", thetas[i], ops[i].to_string().c_str());
 }
