af531d81627ff60b8ec11fd907c6863c554fa111
Melody Ren <melodyr@nvidia.com>
2025-03-12 16:16:47 +0000

    Using pcm generation function in example
    Signed-off-by: Melody Ren <melodyr@nvidia.com>

--- docs/sphinx/examples/qec/python/generate_parity_check_matrix.py
+++ docs/sphinx/examples/qec/python/generate_parity_check_matrix.py
@@ -2,12 +2,6 @@ import cudaq
 import cudaq_qec as qec
 import numpy as np
 
-# Set the number of shots and rounds
-nShots = 30
-nRounds = 5
-distance = 4
-cudaq.set_target("stim")
-
 
 def construct_measurement_error_syndrome(distance, nRounds):
     num_stabilizers = distance - 1
@@ -35,17 +29,18 @@ def construct_measurement_error_syndrome(distance, nRounds):
     return np.array(syndrome_rows).T
 
 
-def get_code_and_pcm(distance, nRounds):
+def get_code_and_pcm(distance, nRounds, Hz=None):
     if nRounds < 2:
         raise ValueError("nRounds must be greater than or equal to 2")
     if distance < 3:
         raise ValueError("distance must be greater than or equal to 3")
 
-    # Create distance-3 repetition code
+    # Create distance n repetition code
     code = qec.get_code("repetition", distance=distance)
 
     # Get the parity check matrix for a single round
-    Hz = code.get_parity_z()
+    if Hz == None:
+        Hz = code.get_parity_z()
     H = np.array(Hz)
 
     # Extends H to nRounds
@@ -66,76 +61,87 @@ def get_code_and_pcm(distance, nRounds):
     return code, H_pcm, Mz
 
 
-code, H_pcm, Mz = get_code_and_pcm(distance, nRounds)
+def main():
+    # Set the number of shots and rounds
+    nShots = 30
+    nRounds = 3
+    distance = 3
+    cudaq.set_target("stim")
 
-# Define the error probability
-p = 0.01
-noise = cudaq.NoiseModel()
-noise.add_all_qubit_channel("x", cudaq.Depolarization2(p), 1)
-noise.add_all_qubit_channel("mz", cudaq.BitFlipChannel(p))
+    code, H_pcm, Mz = get_code_and_pcm(distance, nRounds)
 
-# Prepare the logical |0> state
-statePrep = qec.operation.prep0
-expected_value = 0
+    # Define the error probability
+    p = 0.01
+    noise = cudaq.NoiseModel()
+    noise.add_all_qubit_channel("x", cudaq.Depolarization2(p), 1)
+    noise.add_all_qubit_channel("mz", cudaq.BitFlipChannel(p))
 
-# Sample the memory circuit with noise
-syndromes, data = qec.sample_memory_circuit(code, statePrep, nShots, nRounds,
-                                            noise)
-print("From sample function:\n")
-print("syndromes:\n", syndromes)
-print("data:\n", data)
+    # Prepare the logical |0> state
+    statePrep = qec.operation.prep0
+    expected_value = 0
 
-# Combine nRounds of syndromes per shot into one vector
-syndromes_nrounds = np.array(syndromes).reshape(
-    nShots, nRounds * len(code.get_stabilizers()))
-print(f"syndromes:\n{syndromes_nrounds}")
+    # Sample the memory circuit with noise
+    syndromes, data = qec.sample_memory_circuit(code, statePrep, nShots,
+                                                nRounds, noise)
+    print("From sample function:\n")
+    print("syndromes:\n", syndromes)
+    print("data:\n", data)
 
-# Get observables
-observables = code.get_pauli_observables_matrix()
-Lz = code.get_observables_z()
-print(f"observables:\n{observables}")
-print(f"Lz:\n{Lz}")
-# Pad the observables to be the same dimension as the decoded observable
-Lz_nrounds = np.tile(Lz[0], nRounds)
-Lz_nround_mz = np.pad(Lz_nrounds, (0, Mz.shape[1]), mode='constant')
-print(f"Lz_nround_mz\n{Lz_nround_mz}")
+    # Combine nRounds of syndromes per shot into one vector
+    syndromes_nrounds = np.array(syndromes).reshape(
+        nShots, nRounds * len(code.get_stabilizers()))
+    print(f"syndromes:\n{syndromes_nrounds}")
 
-# Get a decoder
-decoder = qec.get_decoder("single_error_lut", H_pcm)
-nLogicalErrors = 0
+    # Get observables
+    observables = code.get_pauli_observables_matrix()
+    Lz = code.get_observables_z()
+    print(f"observables:\n{observables}")
+    print(f"Lz:\n{Lz}")
+    # Pad the observables to be the same dimension as the decoded observable
+    Lz_nrounds = np.tile(Lz[0], nRounds)
+    Lz_nround_mz = np.pad(Lz_nrounds, (0, Mz.shape[1]), mode='constant')
+    print(f"Lz_nround_mz\n{Lz_nround_mz}")
 
-# Logical Mz each shot (use Lx if preparing in X-basis)
-logical_measurements = (Lz @ data.transpose()) % 2
-# only one logical qubit, so do not need the second axis
-logical_measurements = logical_measurements.flatten()
-print("LMz:\n", logical_measurements)
+    # Get a decoder
+    decoder = qec.get_decoder("single_error_lut", H_pcm)
+    nLogicalErrors = 0
 
-# initialize a Pauli frame to track logical flips
-# through the stabilizer rounds. Only need the Z component for the repetition code.
-pauli_frame = np.array([0, 0], dtype=np.uint8)
+    # Logical Mz each shot (use Lx if preparing in X-basis)
+    logical_measurements = (Lz @ data.transpose()) % 2
+    # only one logical qubit, so do not need the second axis
+    logical_measurements = logical_measurements.flatten()
+    print("LMz:\n", logical_measurements)
 
-for shot, syndrome in enumerate(syndromes_nrounds):
-    print("\nshot:", shot)
-    print("syndrome:", syndrome)
+    # initialize a Pauli frame to track logical flips
+    # through the stabilizer rounds. Only need the Z component for the repetition code.
+    pauli_frame = np.array([0, 0], dtype=np.uint8)
 
-    # Decode the syndrome
-    convergence, result = decoder.decode(syndrome)
-    data_prediction = np.array(result, dtype=np.uint8)
+    for shot, syndrome in enumerate(syndromes_nrounds):
+        print("\nshot:", shot)
+        print("syndrome:", syndrome)
 
-    # See if the decoded result anti-commutes with the observables
-    print("decode result:", data_prediction)
-    decoded_observables = (Lz_nround_mz @ data_prediction) % 2
-    print("decoded_observables:", decoded_observables)
+        # Decode the syndrome
+        convergence, result = decoder.decode(syndrome)
+        data_prediction = np.array(result, dtype=np.uint8)
 
-    # update pauli frame
-    pauli_frame[0] = (pauli_frame[0] + decoded_observables) % 2
-    print("pauli frame:", pauli_frame)
+        # See if the decoded result anti-commutes with the observables
+        print("decode result:", data_prediction)
+        decoded_observables = (Lz_nround_mz @ data_prediction) % 2
+        print("decoded_observables:", decoded_observables)
 
-    corrected_mz = (logical_measurements[shot] + pauli_frame[0]) % 2
-    print("Expected value:", expected_value)
-    print("Corrected value:", corrected_mz)
-    if (corrected_mz != expected_value):
-        nLogicalErrors += 1
+        # update pauli frame
+        pauli_frame[0] = (pauli_frame[0] + decoded_observables) % 2
+        print("pauli frame:", pauli_frame)
 
-# Count how many shots the decoder failed to correct the errors
-print("\nNumber of logical errors:", nLogicalErrors)
+        corrected_mz = (logical_measurements[shot] + pauli_frame[0]) % 2
+        print("Expected value:", expected_value)
+        print("Corrected value:", corrected_mz)
+        if (corrected_mz != expected_value):
+            nLogicalErrors += 1
+
+    # Count how many shots the decoder failed to correct the errors
+    print("\nNumber of logical errors:", nLogicalErrors)
+
+
+if __name__ == "__main__":
+    main()
--- docs/sphinx/examples/qec/python/repetition_code_fine_grain_noise.py
+++ docs/sphinx/examples/qec/python/repetition_code_fine_grain_noise.py
@@ -1,5 +1,11 @@
 import cudaq
+import cudaq_qec as qec
 import numpy as np
+from generate_parity_check_matrix import get_code_and_pcm
+
+nRounds = 3
+nShots = 100
+p = 0.1
 
 
 @cudaq.kernel
@@ -10,34 +16,31 @@ def three_qubit_repetition_code():
     # Initialize the logical |1> state as |111>
     x(data_qubits)
 
-    # Random Bit Flip Errors
-    for i in range(3):
-        cudaq.apply_noise(cudaq.XError, 0.1, data_qubits[i])
+    for i in range(nRounds):
+        # Random Bit Flip Errors
+        for j in range(3):
+            cudaq.apply_noise(cudaq.XError, p, data_qubits[j])
 
-    # Extract Syndromes
-    h(ancilla_qubits)
+        # Extract Syndromes
+        h(ancilla_qubits)
 
-    # First Parity Check
-    z.ctrl(ancilla_qubits[0], data_qubits[0])
-    z.ctrl(ancilla_qubits[0], data_qubits[1])
+        # First Parity Check
+        z.ctrl(ancilla_qubits[0], data_qubits[0])
+        z.ctrl(ancilla_qubits[0], data_qubits[1])
 
-    # Second Parity Check
-    z.ctrl(ancilla_qubits[1], data_qubits[1])
-    z.ctrl(ancilla_qubits[1], data_qubits[2])
+        # Second Parity Check
+        z.ctrl(ancilla_qubits[1], data_qubits[1])
+        z.ctrl(ancilla_qubits[1], data_qubits[2])
 
-    h(ancilla_qubits)
+        h(ancilla_qubits)
 
-    s0 = mz(ancilla_qubits[0])
-    s1 = mz(ancilla_qubits[1])
+        # Measure the ancilla qubits
+        s0 = mz(ancilla_qubits[0])
+        s1 = mz(ancilla_qubits[1])
+        reset(ancilla_qubits[0])
+        reset(ancilla_qubits[1])
 
-    # Correct errors based on syndromes
-    if s0 and s1:
-        x(data_qubits[1])
-    elif s0:
-        x(data_qubits[0])
-    elif s1:
-        x(data_qubits[2])
-
+    # Final measurement to get the data qubits
     mz(data_qubits)
 
 
@@ -48,5 +51,61 @@ noise_model.add_all_qubit_channel("mz", cudaq.BitFlipChannel(0.01))
 # Run the kernel and observe results
 # The percent of samples that are 000 corresponds to the logical error rate
 cudaq.set_target("stim")
-result = cudaq.sample(three_qubit_repetition_code, noise_model=noise_model)
-print(result)
+result = cudaq.sample(three_qubit_repetition_code,
+                      shots_count=nShots,
+                      noise_model=noise_model,
+                      explicit_measurements=True)
+
+# Get the parity check matrix of the repetition code
+Hz = [[1, 1, 0], [0, 1, 1]]
+code, H_pcm, Mz = get_code_and_pcm(3, nRounds, Hz)
+
+# Get observables
+observables = np.array([1, 0, 0, 0, 0, 0], dtype=np.uint8)
+Lz = np.array([1, 0, 0], dtype=np.uint8)
+print(f"observables:\n{observables}")
+print(f"Lz:\n{Lz}")
+# Pad the observables to be the same dimension as the decoded observable
+Lz_nrounds = np.tile(Lz, nRounds)
+Lz_nround_mz = np.pad(Lz_nrounds, (0, Mz.shape[1]), mode='constant')
+print(f"Lz_nround_mz\n{Lz_nround_mz}")
+
+# Get a decoder
+decoder = qec.get_decoder("single_error_lut", H_pcm)
+nLogicalErrors = 0
+
+# initialize a Pauli frame to track logical flips
+# through the stabilizer rounds. Only need the Z component for the repetition code.
+pauli_frame = np.array([0, 0], dtype=np.uint8)
+expected_value = 1
+for shot, outcome in enumerate(result.get_sequential_data()):
+    outcome_array = np.array([int(bit) for bit in outcome], dtype=np.uint8)
+    syndrome = outcome_array[:len(outcome_array) - 3]
+    data = outcome_array[len(outcome_array) - 3:]
+    print("\nshot:", shot)
+    print("syndrome:", syndrome)
+
+    # Decode the syndrome
+    convergence, result = decoder.decode(syndrome)
+    data_prediction = np.array(result, dtype=np.uint8)
+
+    # See if the decoded result anti-commutes with the observables
+    print("decode result:", data_prediction)
+    decoded_observables = (Lz_nround_mz @ data_prediction) % 2
+    print("decoded_observables:", decoded_observables)
+
+    # update pauli frame
+    pauli_frame[0] = (pauli_frame[0] + decoded_observables) % 2
+    print("pauli frame:", pauli_frame)
+
+    logical_measurements = (Lz @ data.transpose()) % 2
+    print("LMz:", logical_measurements)
+
+    corrected_mz = (logical_measurements + pauli_frame[0]) % 2
+    print("Expected value:", expected_value)
+    print("Corrected value:", corrected_mz)
+    if (corrected_mz != expected_value):
+        nLogicalErrors += 1
+
+# Count how many shots the decoder failed to correct the errors
+print("\nNumber of logical errors:", nLogicalErrors)
