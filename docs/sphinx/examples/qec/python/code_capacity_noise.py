49a2124ab5ffa9ed22b60e4e523f87c05b39b5db
Melody Ren <melodyr@nvidia.com>
2025-03-14 17:29:57 +0000

    Hide copyright in doc rendering
    Signed-off-by: Melody Ren <melodyr@nvidia.com>

--- docs/sphinx/examples/qec/python/code_capacity_noise.py
+++ docs/sphinx/examples/qec/python/code_capacity_noise.py
@@ -6,6 +6,7 @@
 # the terms of the Apache License 2.0 which accompanies this distribution.     #
 # ============================================================================ #
 
+# [Begin Documentation]
 import numpy as np
 import cudaq_qec as qec
 
