f84ad9f70b8586296772620dac4b04462e51d414
Ben Howe <bhowe@nvidia.com>
2025-06-30 20:34:41 +0000

    Update test
    Signed-off-by: Ben Howe <bhowe@nvidia.com>

--- libs/qec/python/tests/test_sliding_window.py
+++ libs/qec/python/tests/test_sliding_window.py
@@ -27,28 +27,24 @@ def setTarget():
 def test_sliding_window_1(decoder_name, batched, num_rounds, num_windows):
     cudaq.set_random_seed(13)
     code = qec.get_code('surface_code', distance=5)
-    Lz = code.get_observables_z()
     p = 0.001
     noise = cudaq.NoiseModel()
     noise.add_all_qubit_channel("x", cudaq.Depolarization2(p), 1)
     statePrep = qec.operation.prep0
-    nShots = 200
-
-    # Sample the memory circuit with errors
-    syndromes, data = qec.sample_memory_circuit(code, statePrep, nShots,
-                                                num_rounds, noise)
-
-    logical_measurements = (Lz @ data.transpose()) % 2
-    # only one logical qubit, so do not need the second axis
-    logical_measurements = logical_measurements.flatten()
-
-    # Reshape and drop the X stabilizers, keeping just the Z stabilizers (since this is prep0)
-    syndromes = syndromes.reshape((nShots, num_rounds, -1))
-    syndromes = syndromes[:, :, :syndromes.shape[2] // 2]
-    # Now flatten to two dimensions again
-    syndromes = syndromes.reshape((nShots, -1))
+    nShots = 1000
 
     dem = qec.z_dem_from_memory_circuit(code, statePrep, num_rounds, noise)
+    num_syndromes_per_round = dem.detector_error_matrix.shape[0] // num_rounds
+
+    # Inject only one error per shot. This will keep the number of mismatches
+    # low, and any debug should be straightforward.
+    syndromes = np.zeros((nShots, dem.detector_error_matrix.shape[0]),
+                         dtype=np.uint8)
+    np.random.seed(13)
+    for shot in range(nShots):
+        # Pick a single random error to inject
+        col = np.random.randint(0, dem.detector_error_matrix.shape[1])
+        syndromes[shot, :] = dem.detector_error_matrix[:, col].T
 
     # First compare the results of the full decoder to the sliding window
     # decoder using an inner decoder of the full window size. The results should
@@ -62,7 +58,7 @@ def test_sliding_window_1(decoder_name, batched, num_rounds, num_windows):
         step_size=1,
         num_syndromes_per_round=num_syndromes_per_round,
         straddle_start_round=False,
-        straddle_end_round=False,
+        straddle_end_round=True,
         error_rate_vec=np.array(dem.error_rates),
         inner_decoder_name=decoder_name,
         inner_decoder_params={})
@@ -73,9 +69,7 @@ def test_sliding_window_1(decoder_name, batched, num_rounds, num_windows):
         for r1, r2 in zip(full_results, sw_results):
             if r1.result != r2.result:
                 num_mismatches += 1
-        # The 0.05 is a fudge factor to account for the fact that the results
-        # are not exactly the same when using sliding windows.
-        assert num_mismatches <= (num_windows - 1) * 0.05 * nShots
+        assert num_mismatches == 0
 
     else:
         full_results = []
@@ -86,6 +80,4 @@ def test_sliding_window_1(decoder_name, batched, num_rounds, num_windows):
             r2 = sw_as_full_decoder.decode(syndrome)
             if r1.result != r2.result:
                 num_mismatches += 1
-        # The 0.05 is a fudge factor to account for the fact that the results
-        # are not exactly the same when using sliding windows.
-        assert num_mismatches <= (num_windows - 1) * 0.05 * nShots
+        assert num_mismatches == 0
