2a42d45bf1137647afcebe75cdfe0b9fa8674608
Ben Howe <bhowe@nvidia.com>
2025-10-14 23:43:39 +0000

    Undo debug for tests
    Signed-off-by: Ben Howe <bhowe@nvidia.com>

--- scripts/ci/test_wheels.sh
+++ scripts/ci/test_wheels.sh
@@ -96,12 +96,9 @@ if [ "$package_installed" != "$package_expected" ]; then
   exit 1
 fi
 
-# Prepare for the next test
-${python} -m pip uninstall -y cudaq-solvers cudaq-solvers-cu${cuda_version}
-
 # Test the solvers library with GQE
 echo "Installing Solvers library with GQE"
-${python} -m pip install --no-cache-dir --force-reinstall ${FIND_LINKS} "cudaq-solvers[gqe]"
+${python} -m pip install ${FIND_LINKS} "cudaq-solvers[gqe]"
 ${python} -m pytest -v -s libs/solvers/python/tests/test_gqe.py
 
 # Test the libraries with examples
