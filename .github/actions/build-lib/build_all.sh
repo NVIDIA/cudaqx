405a144f75a65dbebaafd553ddf8636ee3d6b934
Chuck Ketcham <cketcham@nvidia.com>
2026-02-03 00:18:01 +0000

    mock decoder with tests - no cuda graph or autonomous CRTP yet
    Signed-off-by: Chuck Ketcham <cketcham@nvidia.com>

--- .github/actions/build-lib/build_all.sh
+++ .github/actions/build-lib/build_all.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# Download realtime artifacts from GitHub release (if CUDAQ_REALTIME_ROOT not set)
+if [ -z "$CUDAQ_REALTIME_ROOT" ]; then
+  CUDAQ_REALTIME_ROOT=/tmp/cudaq-realtime
+  mkdir -p $CUDAQ_REALTIME_ROOT
+
+  # Download from GitHub release
+  RELEASE_URL="https://github.com/NVIDIA/cudaqx/releases/download/cudaq-realtime-no-push"
+  wget -qO- ${RELEASE_URL}/cudaq-realtime-headers.tar.gz | tar xzf - -C $CUDAQ_REALTIME_ROOT
+
+  ARCH=$(uname -m | sed 's/aarch64/arm64/' | sed 's/x86_64/x86_64/')
+  mkdir -p $CUDAQ_REALTIME_ROOT/lib
+  wget -qO- ${RELEASE_URL}/cudaq-realtime-libs-${ARCH}.tar.gz | tar xzf - -C $CUDAQ_REALTIME_ROOT/lib
+fi
+
 cmake -S . -B "$1" \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_C_COMPILER=gcc-11 \
@@ -10,7 +24,7 @@ cmake -S . -B "$1" \
   -DCUDAQX_ENABLE_LIBS="all" \
   -DCUDAQX_INCLUDE_TESTS=ON \
   -DCUDAQX_BINDINGS_PYTHON=ON \
-  -DCMAKE_INSTALL_PREFIX="$2"
+  -DCMAKE_INSTALL_PREFIX="$2" \
+  -DCUDAQ_REALTIME_ROOT=$CUDAQ_REALTIME_ROOT
 
 cmake --build "$1" --target install
-
