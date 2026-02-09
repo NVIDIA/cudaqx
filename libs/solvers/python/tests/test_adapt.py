7468e421232d32085950c9524e29e76bd52a00b8
Ben Howe <bhowe@nvidia.com>
2026-02-09 12:54:16 -0800

    Skip solvers tests (PYTHON-REFACTOR)
    Signed-off-by: Ben Howe <bhowe@nvidia.com>

--- libs/solvers/python/tests/test_adapt.py
+++ libs/solvers/python/tests/test_adapt.py
@@ -6,13 +6,14 @@
 # the terms of the Apache License 2.0 which accompanies this distribution.     #
 # ============================================================================ #
 
-import os
-
 import pytest
-import numpy as np
+import cudaq_solvers as solvers
+# import os
+
+# import pytest
+# import numpy as np
 
 import cudaq
-import cudaq_solvers as solvers
 
 
 @pytest.mark.skip(reason="PYTHON-REFACTOR")
