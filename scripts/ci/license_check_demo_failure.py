7e15bc43e4b22e6eadf261aee889b21bdf3817a6
Angela Burton <angelab@nvidia.com>
2026-08-28 11:37:11 -0700

    test: add file with non-Apache license header
    Signed-off-by: Angela Burton <angelab@nvidia.com>

---
+++ scripts/ci/license_check_demo_failure.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: GPL-2.0
+#
+# This file exists only to demonstrate that the check-license-headers CI job
+# (see .github/workflows/pr_sanity_checks.yaml) catches a disallowed license
+# header on a changed file. Remove this file before merging.
+
+print("license header check demo")
